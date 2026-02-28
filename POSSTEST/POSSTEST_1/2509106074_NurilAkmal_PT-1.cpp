8c7a0ce6798643149515f91dd0cb6fbf4b931aea
Nuril Akmal <akmalnuril65@gmail.com>
2026-02-28 13:41:01 +0800

    final

--- POSSTEST/POSSTEST_1/2509106074_NurilAkmal_PT-1.cpp
+++ POSSTEST/POSSTEST_1/2509106074_NurilAkmal_PT-1.cpp
@@ -1,6 +1,3 @@
-/// Nama : Nuril Akmal
-/// NIM  : 2509106074   
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -52,4 +49,89 @@ int main() {
         return 0;  
     }
     
-} 
+    int pilihan;
+    double meter, kilometer, centimeter;
+    
+    do {
+        cout << "\n=====================================\n";
+        cout << "           MENU UTAMA\n";
+        cout << "=====================================\n";
+        cout << "1. Konversi Meter -> Kilometer & Centimeter\n";
+        cout << "2. Konversi Kilometer -> Meter & Centimeter\n";
+        cout << "3. Konversi Centimeter -> Meter & Kilometer\n";
+        cout << "4. Keluar\n";
+        cout << "=====================================\n";
+        cout << "Pilih menu (1-4): ";
+        cin >> pilihan;
+        
+        switch (pilihan) {
+            case 1: {
+                cout << "\n--- KONVERSI METER ---\n";
+                cout << "Masukkan nilai Meter: ";
+                cin >> meter;
+                
+                kilometer = meter / 1000;
+                centimeter = meter * 100;
+                
+                cout << "\nHASIL KONVERSI:\n";
+                cout << meter << " Meter\n";
+                cout << "  = " << kilometer << " Kilometer\n";
+                cout << "  = " << centimeter << " Centimeter\n";
+                break;
+            }
+            
+            case 2: {
+                cout << "\n--- KONVERSI KILOMETER ---\n";
+                cout << "Masukkan nilai Kilometer: ";
+                cin >> kilometer;
+                
+                meter = kilometer * 1000;
+                centimeter = kilometer * 100000;
+                
+                cout << "\nHASIL KONVERSI:\n";
+                cout << kilometer << " Kilometer\n";
+                cout << "  = " << meter << " Meter\n";
+                cout << "  = " << centimeter << " Centimeter\n";
+                break;
+            }
+            
+            case 3: {
+                cout << "\n--- KONVERSI CENTIMETER ---\n";
+                cout << "Masukkan nilai Centimeter: ";
+                cin >> centimeter;
+                
+                meter = centimeter / 100;
+                kilometer = centimeter / 100000;
+                
+                cout << "\nHASIL KONVERSI:\n";
+                cout << centimeter << " Centimeter\n";
+                cout << "  = " << meter << " Meter\n";
+                cout << "  = " << kilometer << " Kilometer\n";
+                break;
+            }
+            
+            case 4: {
+                cout << "\n=====================================\n";
+                cout << "     TERIMA KASIH " << nama_input << "!\n";
+                cout << "     Sampai jumpa lagi\n";
+                cout << "=====================================\n";
+                break;
+            }
+            
+            default: {
+                cout << "\nPilihan tidak valid!\n";
+                cout << "Silakan pilih antara 1-4.\n";
+                break;
+            }
+        }
+        
+        if (pilihan != 4) {
+            cout << "\nTekan Enter untuk lanjut...";
+            cin.ignore();
+            cin.get();
+        }
+        
+    } while (pilihan != 4);
+    
+    return 0;
+}
